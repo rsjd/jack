4782daaf561164f8a65a366937528fa6dfd78b58
rsjd <you@example.com>
2019-03-13 16:19:59 +0530

    1st commit

---
+++ src/main/java/com/rajasekar/dao/StudentDAO.java
@@ -0,0 +1,43 @@
+package com.rajasekar.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rajasekar.model.Student;
+import com.rajasekar.repository.StudentRepository;
+
+@Service
+public class StudentDAO {
+	@Autowired
+	StudentRepository studentRepository;
+	
+	/*to save an employee*/
+	
+	public Student save(Student std) {
+		return studentRepository.save(std);
+	}
+	
+	
+	/* search all employees*/
+	
+	public List<Student> findAll(){
+		return studentRepository.findAll();
+	}
+	
+	
+	/*get an employee by id*/
+	public Student findOne(Integer id) {
+		return studentRepository.getOne(id);
+	}
+	
+	
+	/*delete an employee*/
+	
+	public void delete(Student std) {
+		studentRepository.delete(std);
+	}
+	
+
+}
